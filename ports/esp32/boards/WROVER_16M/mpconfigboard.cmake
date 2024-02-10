b156aa257a54fc4c2a4fd9bab0dd6baa45ff3d87
mishafarms <github@mishafarms.us>
2024-02-10 10:09:21 -0800

    Add support for custom OTA partitions on WROVER_16M board
    This commit creates new configuration files to support custom over-the-air (OTA) firmware updates for the WROVER_16M board. It includes configurations to reduce firmware size in order to fit in the OTA partition and also introduces new partition table files for OTA and USER filesystem.
    
    Signed-off-by: mishafarms <github@mishafarms.us>

---
+++ ports/esp32/boards/WROVER_16M/mpconfigboard.cmake
@@ -0,0 +1,29 @@
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    boards/sdkconfig.ble
+    boards/sdkconfig.spiram
+    boards/WROVER_16M/sdkconfig.board
+)
+
+if(MICROPY_BOARD_VARIANT STREQUAL "OTA")
+    set(SDKCONFIG_DEFAULTS
+            ${SDKCONFIG_DEFAULTS}
+            boards/WROVER_16M/sdkconfig.ota
+    )
+
+    list(APPEND MICROPY_DEF_BOARD
+            MICROPY_HW_BOARD_NAME="Wrover 16M module with OTA"
+    )
+endif()
+
+if(MICROPY_BOARD_VARIANT STREQUAL "OTA_USER")
+    set(SDKCONFIG_DEFAULTS
+            ${SDKCONFIG_DEFAULTS}
+            boards/WROVER_16M/sdkconfig.ota_user
+    )
+
+    list(APPEND MICROPY_DEF_BOARD
+            MICROPY_HW_BOARD_NAME="Wrover 16M module with OTA and USER filesys"
+    )
+endif()
+
