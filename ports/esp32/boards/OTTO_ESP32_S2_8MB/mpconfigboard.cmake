d190e9b08ace910323b8b3923a992e7af6f98967
Scotty Franzyshen <sfranzyshen@gmail.com>
2024-03-20 21:59:14 -0700

    Create mpconfigboard.cmake

---
+++ ports/esp32/boards/OTTO_ESP32_S2_8MB/mpconfigboard.cmake
@@ -0,0 +1,19 @@
+set(IDF_TARGET esp32s2)
+
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    ${SDKCONFIG_IDF_VERSION_SPECIFIC}
+    boards/sdkconfig.usb
+    boards/sdkconfig.spiram_sx
+    boards/OTTO_ESP32_S2_8MB/sdkconfig.board
+)
+if(MICROPY_BOARD_VARIANT STREQUAL "OTA")
+    set(SDKCONFIG_DEFAULTS
+            ${SDKCONFIG_DEFAULTS}
+            boards/OTTO_ESP32_S2_8MB/sdkconfig.ota
+    )
+
+    list(APPEND MICROPY_DEF_BOARD
+            MICROPY_HW_BOARD_NAME="ESP32_S2 8M module with OTA"
+    )
+endif()
