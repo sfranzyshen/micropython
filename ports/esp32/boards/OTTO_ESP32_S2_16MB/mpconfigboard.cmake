7291663b29267b1f7b7ef119f2dc1251f0e92acc
Scotty Franzyshen <sfranzyshen@gmail.com>
2024-03-21 01:00:04 -0700

    Delete ports/esp32/boards/OTTO_ESP32_S2_16MB directory

--- ports/esp32/boards/OTTO_ESP32_S2_16MB/mpconfigboard.cmake
+++
@@ -1,9 +0,0 @@
-set(IDF_TARGET esp32s2)
-
-set(SDKCONFIG_DEFAULTS
-    boards/sdkconfig.base
-    ${SDKCONFIG_IDF_VERSION_SPECIFIC}
-    boards/sdkconfig.usb
-    boards/sdkconfig.spiram_sx
-    boards/OTTO_ESP32_S2_16MB/sdkconfig.board
-)
